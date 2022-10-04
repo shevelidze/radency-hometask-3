60e52a29687c2759e4d1709d0762bdacb83bcd6a
Denys Shevel <denys.shevel16@gmail.com>
2022-10-04 16:13:30 -0400

    Finish all the get endpoints

--- web/src/notes/notes.controller.ts
+++ web/src/notes/notes.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { NotesService } from './notes.service';
+
+class NoteNotFoundError extends HttpException {
+  constructor() {
+    super('Failed to find a note with the such id.', HttpStatus.NOT_FOUND);
+  }
+}
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+  constructor(private notesService: NotesService) {}
+  @Get(':id')
+  async getOneNote(@Param('id') id: string) {
+    const note = await this.notesService.getOne(id);
+    if (note === null) throw new NoteNotFoundError();
+    return note;
+  }
+  @Get()
+  async getAllNotes() {
+    return await this.notesService.getAll();
+  }
+}
--- web/src/notes/notes.service.ts
+++ web/src/notes/notes.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { Note } from './note.model';
+import { Category } from './category.model';
+import { InjectModel } from '@nestjs/sequelize';
+
+export class NotesServiceError extends Error {}
+
+export class NonExistingDeletionError extends NotesServiceError {
+  constructor() {
+    super('Deletion of a non-existing note.');
+  }
+}
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  constructor(
+    @InjectModel(Note) private noteModel: typeof Note,
+    @InjectModel(Category) private categoryModel: typeof Category,
+  ) {}
+  getAll() {
+    return this.noteModel.findAll({ attributes: ['id', 'name'] });
+  }
+  getOne(id: string): Promise<Note | null> {
+    return this.noteModel.findByPk(id, {
+      attributes: { exclude: ['categoryId'] },
+    });
+  }
+  async deleteOne(id: string) {
+    const note = await this.noteModel.findByPk(id);
+    if (note === null) throw new NonExistingDeletionError();
+    await note.destroy();
+  }
+  async getStatistics() {
+    const categories = await this.categoryModel.findAll({
+      include: { model: Note, attributes: ['isArchived'] },
+    });
+
+    return categories.map((category) => {
+      return {
+        id: category.id,
+        name: category.name,
+        archived: category.notes.filter((note) => note.isArchived).length,
+        active: category.notes.filter((note) => !note.isArchived).length,
+      };
+    });
+  }
+}
